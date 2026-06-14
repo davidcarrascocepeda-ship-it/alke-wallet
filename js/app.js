833c3b0681d778119b303eeb8d0b84e9e6b4d971
David Carrasco <davidcarrascocepeda@gmail.com>
2026-06-13 20:51:38 -0400

    Implementación de depósitos y actualización de saldo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,63 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+// Creamos un saldo inicial si no existe
+if (!localStorage.getItem("saldo")) {
+    localStorage.setItem("saldo", 250000);
+}
 
-    event.preventDefault();
+const loginForm = document.getElementById("loginForm");
 
-    const usuario = document.getElementById("usuario").value;
-    const password = document.getElementById("password").value;
+if (loginForm) {
 
-    if (usuario === "admin" && password === "1234") {
+    loginForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        const usuario = document.getElementById("usuario").value;
+        const password = document.getElementById("password").value;
+
+        if (usuario === "admin" && password === "1234") {
+
+            window.location.href = "menu.html";
+
+        } else {
+
+            alert("Usuario o contraseña incorrectos");
+
+        }
+
+    });
+
+}
+
+const saldoElemento = document.getElementById("saldo");
+
+if (saldoElemento) {
+
+    const saldo = localStorage.getItem("saldo");
+
+    saldoElemento.textContent = "$" + Number(saldo).toLocaleString("es-CL");
+
+}
+
+const depositForm = document.getElementById("depositForm");
+
+if (depositForm) {
+
+    depositForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        const monto = Number(document.getElementById("montoDeposito").value);
+
+        let saldoActual = Number(localStorage.getItem("saldo"));
+
+        saldoActual += monto;
+
+        localStorage.setItem("saldo", saldoActual);
+
+        alert("Depósito realizado con éxito");
 
         window.location.href = "menu.html";
 
-    } else {
+    });
 
-        alert("Usuario o contraseña incorrectos");
-
-    }
-
-});
+}
